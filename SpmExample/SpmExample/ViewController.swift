337de332104c658a4beeb0726abfe0728d969cec
Caner ÇAKAL <canercakalofficial@gmail.com>
2026-02-16 15:54:48 +0300

    Commit | Kingfisher

--- SpmExample/SpmExample/ViewController.swift
+++ SpmExample/SpmExample/ViewController.swift
@@ -8,55 +8,87 @@
 import UIKit
 import Alamofire
 import SnapKit
+import Kingfisher
 
 class ViewController: UIViewController {
     
     
     @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    //MARK: - SnapKit
-    
-    func setConstraintViaSnapKit() {
-        redView.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.height.equalTo(128.0)
-            make.width.equalTo(256.0)
-        }
-    }
-    func setConstraintViaAutoLayout() {
-        redView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-redView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-redView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-redView.heightAnchor.constraint(equalToConstant: 128.0),
-redView.widthAnchor.constraint(equalToConstant: 256.0)
-        ])
+    //MARK: - Kingfisher
+    func setImageViewViaKingfisher() {
+        imageView.kf.setImage(with: URL(string: "https://images.unsplash.com/photo-1471899236350-e3016bf1e69e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"))
     }
     
-    //MARK: - Alamofire
-    func fetcWithAlamofire() {
-        AF.request("https://httpbin.org/get").response { response in
-            debugPrint(response)
-        }
-    }
-    
-    func fetcWithURLSession() {
-        let url = URL(string: "https://www.stackoverflow.com")
-        let request = URLRequest(url: url!)
+    func setImageViewViaAlamofire() {
+        let url = URL(string: "https://images.unsplash.com/photo-1471899236350-e3016bf1e69e?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+        let cache = NSCache<AnyObject, AnyObject>()
         
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else {
-                return
-            }
-            print(String(data: data, encoding: .utf8) ?? "Data not found or not in correct format!")
+        if let imageFromCache = cache.object(forKey: url as AnyObject) as? UIImage {
+            imageView.image = imageFromCache
+            return
         }
-        task.resume()
+        
+        AF.request(url!, method: .get).response { response in
+            switch response.result {
+            case .failure(let error):
+                print(error)
+            case .success(let data):
+                guard let data = data,
+                      let image = UIImage(data: data) else {
+                    return
+                }
+                cache.setObject(image,
+                                forKey: url as AnyObject)
+                self.imageView.image = image
+            }
+        }
+        //MARK: - SnapKit
+        
+        func setConstraintViaSnapKit() {
+            redView.snp.makeConstraints { make in
+                make.center.equalToSuperview()
+                make.height.equalTo(128.0)
+                make.width.equalTo(256.0)
+            }
+        }
+        func setConstraintViaAutoLayout() {
+            redView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                redView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                redView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                redView.heightAnchor.constraint(equalToConstant: 128.0),
+                redView.widthAnchor.constraint(equalToConstant: 256.0)
+            ])
+        }
+        
+        //MARK: - Alamofire
+        func fetcWithAlamofire() {
+            AF.request("https://httpbin.org/get").response { response in
+                debugPrint(response)
+            }
+        }
+        
+        func fetcWithURLSession() {
+            let url = URL(string: "https://www.stackoverflow.com")
+            let request = URLRequest(url: url!)
+            
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                guard let data = data else {
+                    return
+                }
+                print(String(data: data, encoding: .utf8) ?? "Data not found or not in correct format!")
+            }
+            task.resume()
+        }
+        
+        
     }
-
-
+    
 }
-
